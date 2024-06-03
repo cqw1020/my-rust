8787efd4ceabc0b7053eb6d67b33a94060bfef23
qianwen.chen <qianwen.chen@shopee.com>
2024-06-03 17:48:19 +0800

    achieve a guess game

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # my-rust
 
-cargo run
+开始：cargo new [project_name] 或 cargo init
+
+1、编译&运行：cargo run
+
+2、构建：cargo build
+
+3、查看当前全部依赖的文档：cargo doc --open
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,81 @@
+/**
+* 1
+**/
 // fn main() {
 //     println!("Hello, world!");
 // }
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+/**
+* 2
+**/
+// use ferris_says::say; // from the previous step
+// use std::io::{stdout, BufWriter};
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans!");
+//     let width = message.chars().count();
+
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(&message, width, &mut writer).unwrap();
+// }
+/**
+* 3
+**/
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    // println!("Please input your guess.");
+
+    // let mut guess = String::new();
+
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+    // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    // println!("You guessed: {guess}");
+
+    // match guess.cmp(&secret_number) {
+    //     Ordering::Less => println!("Too small!"),
+    //     Ordering::Greater => println!("Too big!"),
+    //     Ordering::Equal => println!("You win!"),
+    // }
+
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
